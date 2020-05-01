4cd3ce2362b38087343452bf0c0961a018abc296
JulianP911 <60160654+JulianP911@users.noreply.github.com>
2020-05-01 07:44:53 -0500

    Version dos

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,6 @@
 package controller;
 
-import java.util.Iterator;
 import java.util.Scanner;
-
-import model.Comparendo;
-import model.LlaveComparendo2;
 import model.logic.Modelo;
 import view.View;
 
@@ -37,46 +33,15 @@ public class Controller {
 			int option = lector.nextInt();
 			switch(option){
 			case 1:
-				modelo.darArbolRedBlack();
-				view.printMessage("\nLectura de comparendorendos: ");
-				view.printMessage("El numero de comparendos es de: " + modelo.darArbolRedBlack().size());
-				view.printMessage("El comparendo con el mayor OBJECTID es: "+ modelo.darObjectidMayor());
-				view.printMessage("El comparendo con el menor OBJECTID es: "+ modelo.darObjectidMenor() + " \n");
+				
 				break;
 				
 			case 2:
-				view.printMessage("Por favor ingrese el numero del Id del comparendo para ser buscado: ");
-				int pObjectId = lector.nextInt();
-				
-				if(modelo.darComparendoPorObjectID(pObjectId).equals("No hay un comparendo con ese identificador"))
-				{
-					view.printMessage(modelo.darComparendoPorObjectID(pObjectId));
-				}
-				else
-				{
-					view.printMessage("El comparendo es: " + modelo.darComparendoPorObjectID(pObjectId));
-				}
+			
 				break;
 				
 			case 3:
-				view.printMessage("Por favor ingrese el ObjectId Inferior: ");
-				int pObjectInf = lector.nextInt();
-				view.printMessage("Por favor ingrese el ObjectId superior: ");
-				int pObjectSup = lector.nextInt();
-				view.printMessage("Los comparendos que se encuntran dentro de este rango son: ");
 				
-				Iterator<LlaveComparendo2> it1 = modelo.darComparendosEntreDosObjectId(pObjectInf, pObjectSup).keys().iterator();
-				Iterator<Comparendo> it2 = modelo.darComparendosEntreDosObjectId(pObjectInf, pObjectSup).Values().iterator();
-				
-				while(it1.hasNext() && it2.hasNext())
-				{
-					LlaveComparendo2 llaveComparendo =  it1.next();
-					Comparendo Comparendo = it2.next();
-					
-					view.printMessage(llaveComparendo.getObjectid() + ", " + Comparendo.getFecha_hora() + ", " + Comparendo.getTipo_servi() + ", " +
-		                              Comparendo.getClase_vehi() + ", " + Comparendo.getInfraccion());
-					
-				}
 				break;
 
 			case 4:
--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -6,7 +6,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 import com.google.gson.JsonArray;
@@ -15,8 +14,6 @@ import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
 import model.Comparendo;
-import model.LlaveComparendo2;
-import model.data_structures.RedBlackBST;
 
 /**
  * Definicion del modelo del mundo
@@ -27,11 +24,6 @@ public class Modelo
 	public static String PATH = "./data/comparendos_DEI_2018_Bogotá_D.C_small.geojson";
 	//	public static String PATH = "./data/comparendos_DEI_2018_Bogotá_50000_.geojson";
     //	public static String PATH = "./data/comparendos_DEI_2018_Bogotá_D.C.geojson";
-
-	/**
-	 * Lista de comparendos
-	 */
-	private List<Comparendo> datos1;
 
 	/**
 	 * Metodo que hace la carga de los datos comparendos
@@ -85,147 +77,5 @@ public class Modelo
 			e.printStackTrace();
 		}
 		return datos;			
-	}
-
-	/**
-	 * Convierte la lista de objetos cargados a un arbol rojo - negro
-	 */
-	public RedBlackBST<LlaveComparendo2, Comparendo> darArbolRedBlack()
-	{
-		RedBlackBST<LlaveComparendo2, Comparendo> arbolRedBlack = new RedBlackBST<LlaveComparendo2, Comparendo>();
-		datos1 = cargarDatos();
-
-		Iterator<Comparendo> it = datos1.iterator();
-		while(it.hasNext())
-		{
-			for(int i = 0; i < datos1.size(); i++)
-			{
-				Comparendo elementoActual = it.next();
-				LlaveComparendo2 llaveActual = new LlaveComparendo2(elementoActual.getObjective());
-				Comparendo comparendoActual = new Comparendo(elementoActual.getObjective(), elementoActual.getFecha_hora(), elementoActual.getDes_infrac(), elementoActual.getMedio_dete(), elementoActual.getClase_vehi(), elementoActual.getTipo_servi(), elementoActual.getInfraccion(), elementoActual.getLocalidad(), elementoActual.getMunicipio(), elementoActual.getLongitud(), elementoActual.getLatitud());
-				arbolRedBlack.put(llaveActual, comparendoActual);
-			}
-		}
-
-		return arbolRedBlack;
-	}
-	
-	/**
-	 * Muestra la informacion con el mayor OBJECTID encontrado en el arbol rojo - negro
-	 * @return El comparendo con mayor objectid encontrado recorriendo el arbol rojo - negro
-	 */
-	public String darObjectidMayor()
-	{
-		String mensaje = " ";
-		Comparendo actual = datos1.get(0);
-
-		Iterator<Comparendo> it = darArbolRedBlack().Values().iterator();
-		while(it.hasNext())
-		{
-			Comparendo elemento = it.next();
-			if(elemento.getObjective() > actual.getObjective())
-			{
-				actual = elemento;
-			}
-		}
-
-		mensaje = actual.getObjective() + ", " + actual.getFecha_hora() + ", " + actual.getInfraccion() + ", " + 
-				actual.getClase_vehi() + ", " + actual.getTipo_servi() + ", " +  actual.getLocalidad() + ", " +
-				actual.getMunicipio();
-
-		return mensaje;
-	}
-	
-	/**
-	 * Muestra la informacion con el menor OBJECTID encontrado en el arbol rojo - negro
-	 * @return El comparendo con menor objectid encontrado recorriendo el arbol rojo - negro
-	 */
-	public String darObjectidMenor()
-	{
-		String mensaje = " ";
-		Comparendo actual = datos1.get(0);
-
-		Iterator<Comparendo> it = darArbolRedBlack().Values().iterator();
-		while(it.hasNext())
-		{
-			Comparendo elemento = it.next();
-			if(elemento.getObjective() < actual.getObjective())
-			{
-				actual = elemento;
-			}
-		}
-
-		mensaje = actual.getObjective() + ", " + actual.getFecha_hora() + ", " + actual.getInfraccion() + ", " + 
-				actual.getClase_vehi() + ", " + actual.getTipo_servi() + ", " +  actual.getLocalidad() + ", " +
-				actual.getMunicipio();
-
-		return mensaje;
-	}
-	
-	/**
-	 * Metodo que recibe el objectId y se busca en el arbol Rojo - Negro con su llave y su valor 
-	 * @param pObjectId ObjectId del comparendo
-	 * @return El comparendo si se encontro, de lo contrario retorna que no fue encontrado
-	 */
-	public String darComparendoPorObjectID(int pObjectId)
-	{
-		String comparendoEncontrado = " ";
-		Comparendo comparendoObjectId = null;
-		
-		
-		Iterator<LlaveComparendo2> it1 = darArbolRedBlack().keys().iterator();
-		Iterator<Comparendo> it2 = darArbolRedBlack().Values().iterator();
-		
-		while(it1.hasNext() && it2.hasNext())
-		{
-			LlaveComparendo2 llaveComparendo =  it1.next();
-			Comparendo Comparendo = it2.next();
-			
-			if(llaveComparendo.getObjectid() == pObjectId)
-			{
-				comparendoObjectId = Comparendo;
-			}
-			
-		}
-		
-		if(comparendoObjectId == null)
-		{
-			comparendoEncontrado = "No hay un comparendo con ese identificador";
-		}
-		else if(comparendoEncontrado != null)
-		{
-			comparendoEncontrado = comparendoObjectId.getObjective() + ", " + comparendoObjectId.getFecha_hora() + ", " + comparendoObjectId.getTipo_servi() + ", " +
-		                           comparendoObjectId.getClase_vehi() + ", " + comparendoObjectId.getInfraccion();
-		}
-		
-		return comparendoEncontrado;
-	}
-	
-	/**
-	 * Retorna un arbo Rojo - Negro con los comparendos que se encuntran dentro de los rangos ingresados
-	 * @param pObjectInf ObjectID inferior
-	 * @param pObjectSup ObjectID superior
-	 * @return Arbol Rojo - Negro con comparendos dentro del rango
-	 */
-	public RedBlackBST<LlaveComparendo2, Comparendo> darComparendosEntreDosObjectId(int pObjectInf, int pObjectSup)
-	{
-		RedBlackBST<LlaveComparendo2, Comparendo> rangoComparendos = new RedBlackBST<LlaveComparendo2, Comparendo>();
-		
-		Iterator<LlaveComparendo2> it1 = darArbolRedBlack().keys().iterator();
-		Iterator<Comparendo> it2 = darArbolRedBlack().Values().iterator();
-		
-		while(it1.hasNext() && it2.hasNext())
-		{
-			LlaveComparendo2 llaveComparendo =  it1.next();
-			Comparendo Comparendo = it2.next();
-			
-			if(llaveComparendo.getObjectid() >= pObjectInf && llaveComparendo.getObjectid() <= pObjectSup)
-			{
-				rangoComparendos.put(llaveComparendo, Comparendo);
-			}
-			
-		}
-		
-		return rangoComparendos;	
 	}
 }
